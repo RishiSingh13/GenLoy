297cb6e16741066a1b0a2cd9f59a8bf7a2f504a4
RishiSingh13 <rishisingham93@gmail.com>
2026-07-08 17:27:38 +0530

    Update cookie settings for authentication

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -17,8 +17,8 @@ export const gAuth=async (req,res)=>{
       const token= await JsonWebTokenError.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"})
       res.cookie("token",token,{
         httpOnly:true,
-        secure:false,
-        sameSite:"strict",
+        secure:true,
+        sameSite:"none",
         maxAge:7*24*60*60*1000
       })
 
@@ -32,7 +32,13 @@ return res.status(500).json({message:`Google auth error ${error}`})
 export const logout=async(req,res)=>{
 
     try{
-        res.clearCookie("token")
+        
+        res.clearCookie("token",{
+            httpOnly:true,
+            secure:true,
+            samesite:"none"
+            
+        })
 
   return res.status(200).json({message:"logout successfully"})
     }catch(error){
